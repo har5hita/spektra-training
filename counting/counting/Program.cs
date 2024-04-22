bb20f1ab9a2ef2657b15d4722aa80be419d508c8
har5hita <119622674+har5hita@users.noreply.github.com>
2024-04-22 13:08:39 +0530

    Add files via upload

---
+++ counting/counting/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace counting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int counts = 0;
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine().Trim();
+            foreach(char c in str)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    counts++;
+                }
+            }
+            Console.WriteLine((counts+1));
+            Console.ReadLine();
+        }
+    }
+}
