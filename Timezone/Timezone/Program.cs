58cb50a718fb78e19bf96e4dd9e0898d12c31897
har5hita <119622674+har5hita@users.noreply.github.com>
2024-04-22 12:50:30 +0530

    Add files via upload

---
+++ Timezone/Timezone/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Timezone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime locatime= DateTime.Now;
+            Console.WriteLine("Local Time:"+locatime);
+            TimeZoneInfo localTimeZone = TimeZoneInfo.Local;
+            Console.WriteLine($"Time Zone: {localTimeZone.DisplayName}");
+
+           
+            
+        }
+    }
+}
