e60dfd8536b73c1d297395f5fb2bcbbf459a0d8d
har5hita <119622674+har5hita@users.noreply.github.com>
2024-04-22 12:44:04 +0530

    Add files via upload

---
+++ SimpleInterest/SimpleInterest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleInterest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Principal amount");
+            int amt=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the rate of interest in %");
+            int rate=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the time period in years");
+            int time=Convert.ToInt32(Console.ReadLine());
+            double si=((double)(amt*rate*time))/100;
+            Console.WriteLine("Simple Interest:" + si);
+        }
+    }
+}
