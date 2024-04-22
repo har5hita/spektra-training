b6b7ccf5cb26ac2ed05b0f48bc82d101e4569b1b
har5hita <119622674+har5hita@users.noreply.github.com>
2024-04-22 12:42:18 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+       public  class people
+        {
+            protected string name;
+            protected string address;
+            protected int age;
+
+            public void Display()
+            {
+                Console.WriteLine("Name:"+name);
+                Console.WriteLine("Address:" + address);
+                Console.WriteLine("Age:" + age);
+
+            }
+        }
+        class teacher : people
+        {
+            public void getDetails(string n,string add,int Age)
+            {
+                name = n;
+                address = add;
+                age = Age;
+                
+            }
+        }
+        class student : people
+        {
+            public void getDetails(string sn, string sadd, int sAge)
+            {
+                name = sn;
+                address = sadd;
+                age = sAge;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student details:");
+            student s = new student();
+            s.getDetails("harshita", "agra",18);
+            s.Display();
+            Console.WriteLine("Teachers details:");
+            teacher t=new teacher();
+            t.getDetails("Arpita", "patna", 31);
+            t.Display();
+
+        }
+    }
+}
