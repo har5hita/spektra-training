2ecba9543d92f9629e2f88d9df969e3086e5d989
har5hita <119622674+har5hita@users.noreply.github.com>
2024-05-17 15:33:00 +0530

    Add files via upload

---
+++ Fileoperations/Fileoperations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\harsh\\Desktop\\web.txt";
+
+        if (!File.Exists(filePath))
+        {
+            
+            File.Create(filePath);
+            Console.WriteLine("File created at: "+filePath);
+        }
+
+        string existingContent = File.ReadAllText(filePath);
+        Console.WriteLine("Existing Content:\n"+existingContent);
+
+        Console.WriteLine("Enter the content to append:");
+        string newContent = Console.ReadLine();
+
+        File.AppendAllText(filePath, newContent);
+        Console.WriteLine("Content appended successfully.");
+        Console.WriteLine(newContent);
+
+        string updatedContent=File.ReadAllText(filePath);
+        Console.WriteLine("Final contents of file:");
+        Console.WriteLine(updatedContent);
+    }
+
+   
+}
