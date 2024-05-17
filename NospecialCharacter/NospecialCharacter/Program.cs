7059248917ef62ea0fc6d29da086e7e39a4dafd7
har5hita <119622674+har5hita@users.noreply.github.com>
2024-05-17 15:52:54 +0530

    Add files via upload

---
+++ NospecialCharacter/NospecialCharacter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RemoveSpecialCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the text:");
+            string text = Console.ReadLine();
+
+           
+            Regex regex = new Regex("[^a-zA-Z0-9_ -]");
+
+           
+            string result = regex.Replace(text, "");
+
+            Console.WriteLine("Updated string without special characters:");
+            Console.WriteLine(result);
+        }
+    }
+}
