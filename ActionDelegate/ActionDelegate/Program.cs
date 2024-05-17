43f829645fdb7b56f8276750c0f337eb73ba0b8c
har5hita <119622674+har5hita@users.noreply.github.com>
2024-05-17 15:02:36 +0530

    Add files via upload

---
+++ ActionDelegate/ActionDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ActionDelegate
+{
+    using System;
+    using System.Collections.Generic;
+
+    public delegate void PrintItemsAction(IEnumerable<string> items);
+
+    public class ItemPrinter
+    {
+        public static void PrintItems(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> items = new List<string> { "Apple", "Kiwi", "Cherry", "Oranges" };
+
+            PrintItems(items, ItemPrinter.PrintItems);
+        }
+
+            public static void PrintItems(IEnumerable<string> items, PrintItemsAction printAction)
+            {
+                printAction(items);
+            }
+        }
+}
