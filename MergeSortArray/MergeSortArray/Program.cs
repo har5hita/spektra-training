8f725be89b360c19fb7d6b6ca77407560d207be3
har5hita <119622674+har5hita@users.noreply.github.com>
2024-05-17 15:46:24 +0530

    Add files via upload

---
+++ MergeSortArray/MergeSortArray/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace MergeSortArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of both array:");
+            int len=Convert.ToInt32(Console.ReadLine());
+            int []arr1=new int[len];
+            int[]arr2= new int[len];
+            
+            Console.WriteLine("Enter 1st array elements :");
+            for(int i = 0;i < len; i++)
+            {
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter 2nd array elements :");
+            for (int i = 0; i < len; i++)
+            {
+                arr2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] mergedArray = arr1.Concat(arr2).ToArray();
+            Console.WriteLine("Merged array:");
+            foreach (var item in mergedArray)
+            {
+                Console.WriteLine(item);
+            }
+            Array.Sort(mergedArray);
+            Console.WriteLine("Array after sorting in ascending order:");
+            foreach (var item in mergedArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
