576ce9e044f8e01b0b93a122a878ea8bb3f565c5
har5hita <119622674+har5hita@users.noreply.github.com>
2024-05-17 17:12:59 +0530

    Add files via upload

---
+++ prac2/prac2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+        public class Employee
+            {
+            protected string name = "HARSHITA SINGH";
+            protected int age = 22;
+            protected string dept = "IT"; 
+            protected void display()
+            {
+                Console.WriteLine("Employee details\nName:" + name + "\nAge: " + age + "\nDepartment:" + dept);
+            }
+
+        }
+
+        public class Displayinfo:Employee
+        {
+            public void DisplayAge()
+            {
+                Console.WriteLine(age);
+            }
+        }
+        
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Displayinfo d = new Displayinfo();
+        d.DisplayAge();
+    }
+
+}
+        
