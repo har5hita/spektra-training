bbcde62cd64ba89e980f00d799e44d3dc558d73d
har5hita <119622674+har5hita@users.noreply.github.com>
2024-05-17 17:11:02 +0530

    Add files via upload

---
+++ prac1/prac1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace prac1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("array:");
+            int[] arr = { 1, 2, 3, 4, 4, 4, 1, 1, 3 };
+            HashSet<int> set = new HashSet<int>(arr);
+            foreach(int i in set )
+            {
+                Console.WriteLine(i);
+
+            }
+               
+            Console.ReadLine();
+                    }
+    }
+}
